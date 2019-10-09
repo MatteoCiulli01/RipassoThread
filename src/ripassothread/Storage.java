33d571d4d1a7b76ee5dd0ff2e4ad80d8c33410d7
Lombardi Lorenzo <informatica@lai2>
2019-10-09 12:03:53 +0200

    Storage finished

--- src/ripassothread/Storage.java
+++ src/ripassothread/Storage.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ripassothread;
 
-/**
- *
- * @author informatica
- */
-public class Storage {
-    
+class Storage{
+      int  i;
+      public synchronized void setValue(int i){
+            this.i = i;
+      }
+      public synchronized int getValue(){
+            return this.i;
+      }
 }
